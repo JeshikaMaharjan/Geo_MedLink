931bceb2bd42b9454683d5b9f6b83c601a0242df
Jeshika Maharjan <jeshika99@gmail.com>
2023-11-30 22:07:55 +0545

    .

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,5 +1,97 @@
-import {Text} from 'react-native';
+import {Text, StyleSheet, View, Button, PermissionsAndroid} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import MapboxGL from '@rnmapbox/maps';
+import {useEffect, useState} from 'react';
+// import Hooks from '../custom_hooks/hooks';
+
+MapboxGL.setWellKnownTileServer('Mapbox');
+MapboxGL.setAccessToken(
+  'pk.eyJ1Ijoia2NwcmVtIiwiYSI6ImNscGp0cjN4ZzAyeHoyanBjcTg1cHg4Z3oifQ.mF_otRgUhIMfKYdMIouEbg',
+);
+MapboxGL.setConnected(true);
+
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    return granted === 'granted';
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
 
 export default function Map() {
-  return <Text>Map</Text>;
+  const [location, setLocation] = useState(false);
+  // const {getLocation} = Hooks();
+
+  const getLocation = () => {
+    const result = requestLocationPermission();
+    result.then(res => {
+      if (res) {
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            setLocation(position);
+          },
+          error => {
+            console.log(error.code, error.message);
+            setLocation(false);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+    });
+    console.log(location);
+  };
+  useEffect(() => {
+    getLocation();
+  }, []);
+  console.log('$$$', location);
+
+  return (
+    <View style={styles.page}>
+      <View style={styles.container}>
+        {location != false && (
+          <>
+            <MapboxGL.MapView style={styles.map}>
+              <MapboxGL.Camera
+                zoomLevel={15}
+                centerCoordinate={[
+                  location.coords.longitude,
+                  location.coords.latitude,
+                ]}
+                animationMode={'flyTo'}
+                animationDuration={8000}
+              />
+              <MapboxGL.PointAnnotation
+                id="marker"
+                coordinate={[
+                  location.coords.longitude,
+                  location.coords.latitude,
+                ]}>
+                {/* <View /> */}
+              </MapboxGL.PointAnnotation>
+            </MapboxGL.MapView>
+            <Button title="Relocate" onPress={getLocation} />
+          </>
+        )}
+      </View>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  container: {
+    height: 200,
+    width: '100%',
+    flex: 1,
+  },
+  map: {
+    // flex: 1,
+    height: '80%',
+  },
+});
---
+++ custom_hooks/hooks.js
@@ -0,0 +1,41 @@
+import {useState} from 'react';
+import Geolocation from 'react-native-geolocation-service';
+import {PermissionsAndroid} from 'react-native';
+
+export default function Hooks() {
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+      return granted === 'granted';
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  };
+
+  const [location, setLocation] = useState(false);
+
+  const getLocation = () => {
+    const result = requestLocationPermission();
+    result.then(res => {
+      if (res) {
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            setLocation(position);
+          },
+          error => {
+            console.log(error.code, error.message);
+            setLocation(false);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+      return location;
+    });
+    //   console.log(location);
+  };
+  return {getLocation};
+}

bc3bfe9f858a1fdca039d0087ac1d7d8e21f077a
Jeshika Maharjan <jeshika99@gmail.com>
2024-01-11 17:34:57 +0545

    .

--- screens/Map/Map.jsx
+++ screens/Map/Map.jsx
@@ -1,20 +1,12 @@
 import {View} from 'react-native';
 import MapboxGL from '@rnmapbox/maps';
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Mapstyles as styles} from './style/Map';
 import {MAPBOX_TOKEN} from '../../constants/constants';
-import {
-  ActivityIndicator,
-  Button,
-  Card,
-  Searchbar,
-  Text,
-} from 'react-native-paper';
+import {ActivityIndicator, Text} from 'react-native-paper';
 import {GlobalContext} from '../../context/GlobalStates';
 import useHelperFunctions from './utils/helper';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-// import FontistoIcon from 'react-native-vector-icons/FontistoIcon';
+import MapActions from './MapActions';
 
 MapboxGL.setWellKnownTileServer('Mapbox');
 MapboxGL.setAccessToken(MAPBOX_TOKEN);
@@ -23,12 +15,6 @@ MapboxGL.setConnected(true);
 export default function Map() {
   const [{location}, {setLocation}] = useContext(GlobalContext);
   const {getLocation} = useHelperFunctions();
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const onChangeSearch = query => {
-    setSearchQuery(query);
-    console.log(query);
-  };
 
   useEffect(() => {
     getLocation();
@@ -66,71 +52,8 @@ export default function Map() {
                 ]}
               />
             </MapboxGL.MapView> */}
-            <View style={styles.bottomView}>
-              <View style={styles.searchBoxView}>
-                <Searchbar
-                  placeholder="Search location"
-                  onChangeText={onChangeSearch}
-                  value={searchQuery}
-                  style={styles.searchBox}
-                />
-                <FontAwesomeIcon
-                  name="street-view"
-                  size={40}
-                  onPress={getLocation}
-                  color="#1E3050"
-                />
-              </View>
-              <Text variant="titleMedium" style={{color: '#1E3050'}}>
-                Find Nearby
-              </Text>
-              <View style={{flexDirection: 'row', gap: 15}}>
-                <View style={styles.nearbyContainer}>
-                  <Card style={styles.iconContainer}>
-                    <MaterialIcons name="bloodtype" size={40} color="#1E3050" />
-                  </Card>
-                  <Card style={styles.iconContainer}>
-                    <FontAwesomeIcon
-                      name="ambulance"
-                      size={40}
-                      color="#1E3050"
-                    />
-                  </Card>
-                </View>
-                <Card style={styles.buttonContainer}>
-                  <Button
-
-                  // onPress={() => {
-                  //   pass;
-                  // }}
-                  >
-                    Send Request
-                  </Button>
-                </Card>
-              </View>
-            </View>
+            <MapActions />
           </>
-          // ------------------------------------
-          // <>
-          //   <MapboxGL.MapView style={styles.map}>
-          //     <MapboxGL.Camera
-          //       zoomLevel={15}
-          //       centerCoordinate={[
-          //         location.coords.longitude,
-          //         location.coords.latitude,
-          //       ]}
-          //       animationMode={'flyTo'}
-          //       animationDuration={8000}
-          //     />
-          //     <MapboxGL.PointAnnotation
-          //       id="marker"
-          //       coordinate={[
-          //         location.coords.longitude,
-          //         location.coords.latitude,
-          //       ]}
-          //     />
-          //   </MapboxGL.MapView>
-          // </>
         )}
       </View>
     </View>
---
+++ screens/Map/MapActions.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import {Button, Card, Searchbar, Text} from 'react-native-paper';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {Mapstyles as styles} from './style/Map';
+import useHelperFunctions from './utils/helper';
+
+const MapActions = () => {
+  const {getLocation} = useHelperFunctions();
+  const [searchText, setSearchText] = useState('');
+
+  const onChangeSearch = query => {
+    setSearchText(query);
+    console.log(query);
+  };
+  return (
+    <View style={styles.bottomView}>
+      <View style={styles.searchBoxView}>
+        <Searchbar
+          placeholder="Search location"
+          onChangeText={onChangeSearch}
+          value={searchText}
+          style={styles.searchBox}
+        />
+        <FontAwesomeIcon
+          name="street-view"
+          size={40}
+          onPress={getLocation}
+          color="#1E3050"
+        />
+      </View>
+      <Text variant="titleMedium" style={{color: '#1E3050'}}>
+        Find Nearby
+      </Text>
+      <View style={{flexDirection: 'row', gap: 15}}>
+        <View style={styles.nearbyContainer}>
+          <Card style={styles.iconContainer}>
+            <MaterialIcons name="bloodtype" size={40} color="#1E3050" />
+          </Card>
+          <Card style={styles.iconContainer}>
+            <FontAwesomeIcon name="ambulance" size={40} color="#1E3050" />
+          </Card>
+        </View>
+        <Card style={styles.buttonContainer}>
+          <Button
+
+          // onPress={() => {
+          //   pass;
+          // }}
+          >
+            Send Request
+          </Button>
+        </Card>
+      </View>
+    </View>
+  );
+};
+
+export default MapActions;

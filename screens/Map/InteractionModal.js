d40b137d48415a76afba4e834692bd30a18a368e
Jeshika Maharjan <jeshika99@gmail.com>
2024-01-28 12:18:07 +0545

    .

--- screens/Map/InteractionModal.js
+++ screens/Map/InteractionModal.js
@@ -1,16 +1,46 @@
 import React, {useContext, useState} from 'react';
 import {View} from 'react-native';
-import {Button, Text} from 'react-native-paper';
+import {Button, RadioButton, Text} from 'react-native-paper';
 import {Mapstyles as styles} from './style/Map';
 import {GlobalContext} from '../../context/GlobalStates';
 
 const InteractionModal = ({navigation}) => {
-  const [, {setIsInteractionModalVisible}] = useContext(GlobalContext);
+  const [{userName, location}, {setIsInteractionModalVisible}] =
+    useContext(GlobalContext);
   const [modalNumber, setModalNumber] = useState(0);
+  const [bloodGroup, setBloodGroup] = useState();
   const navigate = navigation.navigate;
+  const [isError, setIsError] = useState(false);
+
+  async function postData() {
+    setIsError(false);
+    const data = {
+      bloodGroup: bloodGroup,
+      latitude: location?.coords?.latitude,
+      longitude: location?.coords?.longitude,
+      // userName: userName,
+      userName: 'test',
+    };
+    console.log(data);
+    setModalNumber(1);
+    // try {
+    //   const res = await axios.post(`http://${baseURL}/api/send/notifications/request`, data);
+
+    //   if (!res) throw new Error();
+    //   setModalNumber(1);
+
+    // } catch (error) {
+    //   console.log(error?.response?.data);
+
+    // }
+  }
 
   const handleBloodDonorClick = () => {
-    setModalNumber(1);
+    if (!bloodGroup) {
+      setIsError(true);
+      return;
+    }
+    postData();
     // API logic
   };
 
@@ -22,6 +52,29 @@ const InteractionModal = ({navigation}) => {
     <View style={styles.modalInnerContainer}>
       {modalNumber === 0 ? (
         <>
+          <Text>Select Blood Group</Text>
+
+          <View>
+            <RadioButton.Group
+              onValueChange={value => {
+                setBloodGroup(value);
+              }}
+              value={bloodGroup}>
+              <RadioButton.Item label="A-" value="A-" />
+              <RadioButton.Item label="A+" value="A+" />
+              <RadioButton.Item label="B-" value="B-" />
+              <RadioButton.Item label="B+" value="B+" />
+              <RadioButton.Item label="AB-" value="AB-" />
+              <RadioButton.Item label="AB+" value="AB+" />
+              <RadioButton.Item label="O-" value="O-" />
+              <RadioButton.Item label="O+" value="O+" />
+            </RadioButton.Group>
+          </View>
+          {isError && (
+            <Text style={{color: 'red'}}>
+              Please choose a blood group to continue.
+            </Text>
+          )}
           <Text>Please choose your desired request</Text>
           <Button mode="elevated" onPress={handleBloodDonorClick}>
             Request Blood Donor
